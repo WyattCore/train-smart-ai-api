542666f937c6a9f66802e95422da6a28a327a1b1
KrispyKremeMan <Wyatt.core@yahoo.com>
2025-03-04 12:31:11 -0600

    fixes

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,8 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+const express = require("express");
+const app = express();
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
-}
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
